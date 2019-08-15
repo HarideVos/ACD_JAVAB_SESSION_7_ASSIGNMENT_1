3e2dfd332420bf9287151d1f97943ad92a50f304
HarideVos <53867807+HarideVos@users.noreply.github.com>
2019-08-15 09:04:35 -0400

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,11 @@
+package sevenpointone;
+
+public class Convert {
+	public static void main(String args[]) {
+		int a = 10;
+		StringBuilder sb = new StringBuilder();
+		sb.append(a);
+		String number = sb.toString();
+		System.out.print(number);
+	}
+}
